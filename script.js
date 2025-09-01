a1de5bca18888e5a6f1d896c9e19d325fc80ed84
Highbee3 <ibrosoft43@gmail.com>
2025-09-01 06:22:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+// console.log ('Hello world')
+
+
+// variables
+
+// let name = ('ibrahim')
+// console.log (name)
+
+// if(true){
+//     name = "Olalekan"
+// }
+
+// console.log(name)
+
+// console.log(name)
+
+// let name = "olalekan"
+
+  function showAlert() {
+      document.getElementById("overlay").style.display = "flex";
+    }
+
+    function closeAlert() {
+      document.getElementById("overlay").style.display = "none";
+    }
